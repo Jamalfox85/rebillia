98e59a59ac458f218c34dfb0ffdc7656a980b08e
Jamal <Fox.Jamal@outlook.com>
2021-11-25 00:55:34 -0500

    build out further index-hero elements

--- src/components/INDEX_HERO/IndexHero.js
+++ src/components/INDEX_HERO/IndexHero.js
@@ -1,15 +1,120 @@
 import React from "react"
 import "./IndexHero.css"
+import Particles from "react-tsparticles"
 
 const index_hero = () => {
+  const particlesInit = main => {
+    console.log(main)
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  }
+
+  const particlesLoaded = container => {
+    console.log(container)
+  }
+
   return (
     <div className="index_hero">
-      <div className='index_hero_left'>
+      <div className="index_hero_left">
         <h1>The subscription network that keeps you connected.</h1>
-        <div className='particle-js'></div>
-        <button className='button-purple_pos'>Contact</button>
+        <div className="particle-sphere">
+          <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            className="particles"
+            width="500px"
+            height="500px"
+            options={{
+              background: {
+                color: {
+                  value: "#0d47a1",
+                },
+              },
+              fpsLimit: 60,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40,
+                  },
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#ffffff",
+                },
+                links: {
+                  color: "#ffffff",
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outMode: "bounce",
+                  random: false,
+                  speed: 6,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    value_area: 800,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  random: true,
+                  value: 5,
+                },
+              },
+              detectRetina: true,
+            }}
+          />
+        </div>
+        <button className="button-purple_pos">Contact</button>
       </div>
-      <div className='index_hero_right'></div>
+      <div className="index_hero_right">
+        <div className="card">
+          <h2 className="card-logo">Rebillia</h2>
+          <h2 className="card-number">4242 4242 4242 4242</h2>
+          <h2 className="card-expiration">12/25</h2>
+          <h2 className="card-name">Rebillia Platform</h2>
+          <h2 className="card-visa">VISA</h2>
+        </div>
+      </div>
     </div>
   )
 }
