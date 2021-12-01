eaaef41f27ced234c60d9475bac9c35c6b94003d
Jamalfox85 <Fox.Jamal@outlook.com>
2021-12-01 00:31:24 -0500

    developed slideshow feature for features section

--- src/components/INDEX_FEATURES/IndexFeatures.js
+++ src/components/INDEX_FEATURES/IndexFeatures.js
@@ -1,17 +1,48 @@
-import React from "react"
+import React, { useState } from "react"
 import "./IndexFeatures.css"
 import { StaticImage } from "gatsby-plugin-image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCheckSquare } from "@fortawesome/free-solid-svg-icons"
+import {onlinepayments} from '../../Assets/onlinepayments.png'
+import {popularpurchases} from '../../Assets/popularpurchases.png'
 
 const IndexFeatures = () => {
+  const featureTitles = [
+    "Recurring Offerings",
+    "Popular Purchases",
+    "Customer Lifecycles",
+    "Detailed Reporting",
+  ]
+  const featureImages = [
+    onlinepayments,
+    popularpurchases,
+    "../../Assets/customerlifecycles.png",
+    "../../Assets/detailedreporting.png",
+  ]
+
+  const [featureTitleIndex, setFeatureTitleIndex] = useState(0)
+
+  const updateFeatureTitle = () => {
+    setTimeout(() => {
+      if (featureTitleIndex < featureTitles.length - 1) {
+        setFeatureTitleIndex(prevFeatureTitleIndex => prevFeatureTitleIndex + 1)
+      } else {
+        setFeatureTitleIndex(0)
+      }
+    }, 2500)
+  }
+  updateFeatureTitle()
+
   return (
     <div className="index_features_wrapper">
       <div className="index_features_content">
         <div className="index_features_left">
           <h5>Stay Connected To...</h5>
-          <h1>Finances</h1>
-          <StaticImage src="../../Assets/onlinepayments.png" />
+          <h1>{featureTitles[featureTitleIndex]}</h1>
+          <StaticImage
+            alt="online payments"
+            src={featureImages[featureTitleIndex]}
+          />
         </div>
         <div className="index_features_right">
           <ul>
